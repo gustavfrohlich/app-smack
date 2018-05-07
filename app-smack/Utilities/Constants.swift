a883cd1dcd50b32845d7e3adafca2376663bde85
Gustav <gustav@Gustavs-MacBook-Air.local>
2018-05-07 14:36:43 +0200

    created the service

--- app-smack/Utilities/Constants.swift
+++ app-smack/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL constants
+let BASE_URL = "https://app-smack-gustav.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATEACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//User defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
